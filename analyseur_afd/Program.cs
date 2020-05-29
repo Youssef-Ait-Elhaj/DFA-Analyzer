99e35e5947c105898e4de0267360c304b4c9906d
Void <qpinoza@gmail.com>
2020-05-29 17:17:24 +0100

    finished basic functionality code, still some bugs to resolve

--- analyseur_afd/Program.cs
+++ analyseur_afd/Program.cs
@@ -6,11 +6,17 @@ namespace analyseur_afd
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Entrez un nom de fichier: ");
-            // string fileName = Console.ReadLine();
-            AFD afd = AFD.read("/home/youssef/RiderProjects/analyseur_afd/analyseur_afd/test3.txt");
+            Console.WriteLine("Entrez le nom de fichier:");
+            string fileName = Console.ReadLine();
+            string path = "/home/youssef/RiderProjects/analyseur_afd/analyseur_afd/" + fileName;
+            AFD afd = AFD.read(path);
             AFD.print(afd);
-            Console.WriteLine(AFD.accept(afd,"babba"));
+            Console.WriteLine("Entrez un mot:");
+            string w = Console.ReadLine();
+            if (AFD.accept(afd, w))
+                Console.WriteLine("Le mot {0} est accepté par la language de l'automate!", w);
+            else
+                Console.WriteLine("Le mot {0} n'est pas accepté par la language de l'automate!", w);
         }
     }
 }
